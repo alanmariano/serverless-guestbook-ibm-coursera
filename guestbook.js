4be7277bd8fa4b24d297cb74b3d57ac3ce5b4064
Alan Mariano <alanmariano96@gmail.com>
2022-07-01 20:11:00 -0300

    Updating baseURL
    Updating baseURL to add my api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4a9dcc0c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
